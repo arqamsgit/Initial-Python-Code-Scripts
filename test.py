52d9a643165c3b371d02965bb2b5fe9230c80435
arqamsgit <arqam1878@gmail.com>
2025-08-30 22:57:19 +0500

    Added first if else

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 print("Hello World!")
 print("Hello AI!!!")
+print(123)
+
+A = "Arqam"
+B = "Nisar"
+
+if A == B:
+    "First name is same as last name"
+else:
+    "First name and last name are different."
